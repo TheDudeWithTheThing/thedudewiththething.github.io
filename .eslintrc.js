5ddd1ecefb59573d3ada3e6fd17aeccf1aebb2dd
Shaun Butler <shaun.butler@gmail.com>
2018-09-20 15:57:53 -0400

    Adds .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    "extends": "airbnb-base",
+    "no-alert": false,
+    "globals": {
+      "document": true,
+      "localStorage": true,
+      "moment": true,
+      "window": true,
+    }
+};
